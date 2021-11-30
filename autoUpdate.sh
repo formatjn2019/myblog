7797dd219a3e8ddd1e079dbe9486300868390213
formatjn2019 <50687013+formatjn2019@users.noreply.github.com>
2021-11-30 15:58:28 +0800

    Update autoUpdate.sh

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -2,8 +2,13 @@
 cd /home/zx/blog/myblog
 # 更新
 git pull
-# 重新生成readme和config文件
-python3 create_config_and_indexs.py
-# 重启docker下的vuepress
-docker stop vuepress
-docker run vuepress
+# 由于国内网络问题，增加成功判定
+if [ $? -ne 0]; then
+    echo "拉取失败"
+else
+    # 重新生成readme和config文件
+    python3 create_config_and_indexs.py
+    # 重启docker下的vuepress
+    docker stop vuepress
+    docker run vuepress
+fi
